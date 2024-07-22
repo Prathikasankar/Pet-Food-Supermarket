d2baa640fdb742e0eb44320a73d515be367544d8
Prathika S <163441188+Prathikasankar@users.noreply.github.com>
2024-07-22 18:08:09 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Customer,Product,Orders,Feedback,Category
+
+class CategoryAdmin (admin.ModelAdmin):
+    pass
+admin.site.register(Category,CategoryAdmin)
+
+# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Customer, CustomerAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Product, ProductAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Orders, OrderAdmin)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Feedback, FeedbackAdmin)
+# Register your models here.
